f21372d3aaa42552237f8e1a316c61c3afde498c
GuiZ <Guido Camerlingo>
2024-07-31 10:40:36 +0200

    some data functions

--- src/main.py
+++ src/main.py
@@ -5,10 +5,22 @@ def main():
     this function to be used mainly to run the applicaiton via command line and for testing
     """
 
-    pa = pandasArrow('/Users/marco/code/dataset-explorer/examples/crx.csv')
+    pa = pandasArrow('../examples/crx.csv', header=None)
     pa.head()
     print(pa.dtype())
 
+    print("Numeric DF")
+    print(pa.identify_numeric())
+    
+    print("DropNA")
+    print(pa.dropna())    
+  
+    # Fill NaN values with the mean of each column
+    print("Fill NaN values with the mean of each column")
+    print(pa.fillna_with_mean_of_column())   
+
+    print("Corr Matrix")
+    print(pa.corr())   
 
 if __name__ == "__main__":
     main()
--- src/pandasArrow.py
+++ src/pandasArrow.py
@@ -3,11 +3,28 @@ import pyarrow as pa
 
 class pandasArrow():
 
-    def __init__(self, data) -> None:
-        self.df = pd.read_csv(data, dtype_backend="pyarrow")
+    def __init__(self, data, header) -> None:
+        self.df = pd.read_csv(data, dtype_backend="pyarrow", header=header)
 
     def head(self) -> pd.DataFrame:
         print(self.df.head())
 
     def dtype(self) -> object:
-        return self.df.dtypes
+        return self.df.dtypes 
+
+
+    def dropna(self) -> object:
+        self.df = self.df.dropna(axis=0, how='any')
+        self.df = self.df.dropna(axis=1, how='any')
+        return self.df
+
+    def identify_numeric(self) -> object:
+        self.df = self.df.apply(pd.to_numeric, errors='coerce')
+        return self.df
+    
+    def fillna_with_mean_of_column(self) -> object:
+        self.df = self.df.fillna(self.df.mean())
+        return self.df
+    
+    def corr(self) -> object:
+        return self.df.corr()
